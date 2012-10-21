2c43ce223c1b6b98dc4fa650b483005a938cb22b
Andreas Zecher <andreas@madebypixelate.com>
2012-10-21 22:03:52 +0200

    Added password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :find_events
+  before_filter :authenticate
+  
+  private
   
   def find_events
     @events = Event.order("year DESC")
+  end
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "igf2013preview"
+    end
   end
 
 end
